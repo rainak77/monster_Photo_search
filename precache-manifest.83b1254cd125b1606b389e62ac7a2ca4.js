862dd907fc470b9d6e51bbd8b5028b75826ce3d9
md syedul arif <raiak_77@yahoo.com>
2020-10-09 11:46:58 +0300

    Updates

---
+++ precache-manifest.83b1254cd125b1606b389e62ac7a2ca4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b957e47bb08caf07269a3a61bdd61650",
+    "url": "/react_monster/index.html"
+  },
+  {
+    "revision": "75f66354d8a6970c7da3",
+    "url": "/react_monster/static/css/main.0bc4d8d5.chunk.css"
+  },
+  {
+    "revision": "c01857332f0d1299388e",
+    "url": "/react_monster/static/js/2.558d97e3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_monster/static/js/2.558d97e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75f66354d8a6970c7da3",
+    "url": "/react_monster/static/js/main.ef77cb40.chunk.js"
+  },
+  {
+    "revision": "ee5206f5f6f04ff6e371",
+    "url": "/react_monster/static/js/runtime-main.730b9694.js"
+  }
+]);
